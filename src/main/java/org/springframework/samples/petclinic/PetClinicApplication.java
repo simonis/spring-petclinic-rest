3ec759bb16d3cd888a1febc9f590766eb250cb44
Volker Simonis <volker.simonis@gmail.com>
2024-01-19 20:48:57 +0100

    Define new property '-DPetClinicApplication.shutDown' which immediately shuts down the application (useful for startup measurements)

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PetClinicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(PetClinicApplication.class, args);
+		if (Boolean.getBoolean("PetClinicApplication.shutDown")) {
+			ctx.close();
+		}
 	}
 }
